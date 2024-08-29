71c47102d07502b6613081dce8a0bb3cd7d13456
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-08-30 00:44:58 +0800

    fix dotenv config

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: String(process.env.DB_PASSWORD),
     host : process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: process.env.DB_PORT
+    database: process.env.DB_NAME
 })
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
+require('dotenv').config();
 
 //middleware
 app.use(express.json())
