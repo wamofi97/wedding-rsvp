a7dfab4c566ee84c1154ca8ce294ab5b6822b4c3
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-09-23 19:48:12 +0800

    add configuration for routing issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
