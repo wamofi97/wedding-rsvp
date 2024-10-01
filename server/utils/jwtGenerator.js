b66fd158107e7d3c954b544e9da1e714083bd0bf
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-10-01 22:07:17 +0800

    update jwt expired hours

--- server/utils/jwtGenerator.js
+++ server/utils/jwtGenerator.js
@@ -6,7 +6,7 @@ const jwtGenerator = (user_id) =>{
         user: user_id
     }
 
-    return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "3h"})
+    return jwt.sign(payload, process.env.jwtSecret, {expiresIn: "24h"})
 }
 
 module.exports = jwtGenerator
