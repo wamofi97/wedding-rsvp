8631683ec11b539ed826629eee7690760512b611
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-10-02 10:59:22 +0800

    update separatetime

--- client/src/components/Aturcara.jsx
+++ client/src/components/Aturcara.jsx
@@ -4,17 +4,22 @@ const Aturcara = ({program}) => {
 
     const { ref:inViewRef, inView } = useInView({threshold: 0.5, triggerOnce: true});
     
+        const separateTime = (time) => {
+        const [hours, minutes] = time.split(":");
+        return [hours, minutes];
+    }
+
     return (
     <div  className= "w-full p-12">
         <div ref={inViewRef} className= {inView ? "animate-scroll" : "opacity-0"}>
             <h5 className="font-medium mb-4 text-center text-rose-950" >Aturcara</h5>
             {program &&
                 program.programs.map((program) => {
-                    const separateTime = (program.time).split(":");
-                    const ampm = separateTime[0] > 11 ? "PM" : "AM"; 
+                    const [hours, minutes] = separateTime(program.time);
+                    const ampm = hours > 11 ? "PM" : "AM"; 
                     return (
                     <div key={program.activity} className="flex flex-col items-center mb-8">
-                        <p className="">{program.time} {ampm}:</p>
+                        <p className="">{hours}:{minutes} {ampm}:</p>
                         <p className="capitalize font-medium text-center">{program.activity}</p>
                     </div>
                     )})
@@ -23,5 +28,6 @@ const Aturcara = ({program}) => {
     </div>
   )
 }
+
 
 export default Aturcara
