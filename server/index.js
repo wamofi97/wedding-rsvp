8684f8109885b4e29f3da70b45ab9f9541a101db
Wan Mohd Firdaus <wamofi97@gmail.com>
2024-10-19 04:26:55 +0800

    fly.io config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 require('dotenv').config();
 
 //middleware
